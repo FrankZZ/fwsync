fc4930e006d547d0d81252174f35c3f27b1e4404
FrankZZ <frank@wamm.es>
2014-01-15 19:28:27 +0100

    Added GET command handler

--- fwsync-server/GetCommandHandler.cpp
+++ fwsync-server/GetCommandHandler.cpp
@@ -1,4 +1,6 @@
 #include "GetCommandHandler.h"
+using namespace std;
+
 namespace fwsync
 {
 
@@ -16,7 +18,30 @@ namespace fwsync
 
 	void GetCommandHandler::process(Socket* socket, std::vector<std::string>& params)
 	{
+		ifstream isFile(params[1], ifstream::binary);
 
+		char* buff = new char[BUFFERSIZE + 1];
+
+		streampos end;
+
+		isFile.seekg(0, isFile.end);
+		end = isFile.tellg();
+		isFile.seekg(0, isFile.beg);
+		
+		socket->writeline(to_string(end).c_str());
+
+		if (!isFile.is_open())
+		{
+			throw("cannot open file");
+		}
+
+		while (isFile.good())
+		{
+			isFile.get(buff, BUFFERSIZE);
+			socket->write(buff);
+		}
+
+		delete[] buff;
 	}
 
 	CommandHandler* GetCommandHandler::clone()
--- fwsync-server/GetCommandHandler.h
+++ fwsync-server/GetCommandHandler.h
@@ -1,21 +1,26 @@
 #pragma once
 #include "CommandHandler.h"
 #include <string>
+#include <iostream>
+#include <fstream>
+#include "constants.h"
+
+//#include <stdlib.h>
 
 namespace fwsync
 {
 
 	class GetCommandHandler : public CommandHandler
 	{
-	private:
-		GetCommandHandler();
-		GetCommandHandler(std::string);
+		private:
+			GetCommandHandler();
+			GetCommandHandler(std::string);
 
-	public:
-		virtual void process(Socket* socket, std::vector<std::string>& params);
-		virtual CommandHandler* clone();
+		public:
+			virtual void process(Socket* socket, std::vector<std::string>& params);
+			virtual CommandHandler* clone();
 
-	private:
-		static GetCommandHandler m_getCmdHandler;
+		private:
+			static GetCommandHandler m_getCmdHandler;
 	};
 }
